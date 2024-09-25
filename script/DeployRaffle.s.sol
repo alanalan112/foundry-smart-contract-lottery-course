a25037ce723d76f34a32baa01bcc3829a75d965e
Alan <alanalan112@gmail.com>
2024-09-25 15:07:57 +0800

    test added

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "script/Interaction.s.sol";
+
+contract DeployRaffle is Script {
+    function run() public returns (Raffle, HelperConfig) {
+        return deployContract();
+    }
+
+    function deployContract() public returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        // local -> deploy mock & use mock config
+        // sepolia -> use sepolia config
+
+        if (config.subscriptionId == 0) {
+            // if no subscription id -> create subscription -> fund it -> add consumer
+            /* Create Subscription */
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) = createSubscription
+                .createSubscription(config.vrfCoordinator, config.account);
+            /* Fund */
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                config.vrfCoordinator,
+                config.subscriptionId,
+                config.link,
+                config.account
+            );
+        }
+
+        vm.startBroadcast(config.account);
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        /* Add Consumer */
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(raffle),
+            config.vrfCoordinator,
+            config.subscriptionId,
+            config.account
+        );
+
+        return (raffle, helperConfig);
+    }
+}
---
+++ test/integration/Interactions.t.sol
@@ -0,0 +1,47 @@
+// unit
+// integrations
+// forked
+// staging <- run tests on a mainnet or testnet
+
+// fuzzing
+// stateful fuzz
+// stateless fuzz
+// formal verification
+
+// SPDX-License-Identidier: MIT
+
+pragma solidity 0.8.19;
+
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {Test, console} from "lib/forge-std/src/Test.sol";
+import {Vm} from "lib/forge-std/src/Vm.sol";
+import {StdCheats} from "lib/forge-std/src/StdCheats.sol";
+import {VRFCoordinatorV2_5Mock} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+contract Interactions is StdCheats, Test {
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+
+    uint256 subscriptionId;
+    bytes32 gasLane;
+    uint256 interval;
+    uint256 entranceFee;
+    uint32 callbackGasLimit;
+    address vrfCoordinator;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.run();
+        vm.deal(PLAYER, STARTING_USER_BALANCE);
+
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        subscriptionId = config.subscriptionId;
+        gasLane = config.gasLane;
+        interval = config.interval;
+    }
+}
